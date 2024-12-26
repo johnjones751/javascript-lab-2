5cb064be57d8d1d9584a8eb9eb5d1237dbe53578
johnjones751 <johnjonesjr1121@gmail.com>
2024-12-26 17:49:01 -0600

    week5lab2

---
+++ script.js
@@ -0,0 +1,11 @@
+"use script";
+
+function determineSmallestNum(a, b, c) {
+  if (a < b && a < c) {
+    alert(a + "is th smallest");
+  } else if (b < a && b < C) {
+    alert(b + "is the smallest");
+  } else if (c < a && c < b) {
+    alert(c + "is the smallest");
+  }
+}
