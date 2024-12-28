0fc2f6849334b868270a5ba823b5d92bed757afb
johnjones751 <johnjonesjr1121@gmail.com>
2024-12-28 16:56:06 -0600

    finalCommit

--- script.js
+++ script.js
@@ -10,4 +10,28 @@ function determineSmallestNum(a, b, c) {
   }
 }
 
-determineSmallestNum(4, 6, 1);
+determineSmallestNum(4, 63, 17);
+
+function negativePositiveOrZero(number) {
+  if (number > 0) {
+    alert("Positive");
+  } else if (number === 0) {
+    alert("Zero");
+  } else if (number < 0) {
+    alert("Negative");
+  }
+}
+let userInput = prompt("Enter A Number");
+
+negativePositiveOrZero(userInput);
+
+function votingAge(age) {
+  if (age < 18) {
+    alert("Must be 18 to vote");
+  } else if (age >= 18) {
+    alert("You can vote");
+  }
+}
+
+userInput = prompt("What is your age?");
+votingAge(userInput);
